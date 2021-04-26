d6054b28dd7f1a397377dfd49339bae66f143bfb
Vivek Tiwari <vt2174781@gmail.com>
2021-04-26 12:20:03 +0530

    Done with Authentication and fetching data from json ( 26-04-2021 )

--- main.py
+++ main.py
@@ -1,35 +1,96 @@
-import Authentication
-import login_successfull
+# importing the module
+import json
 
-dictionary = {"Bucky": "11111", "Alita": "22222"}
+def do_you_want_to_contimue():
+    pass
 
 
-print("IF YOU ARE NEW ON THIS PLEASE REGISTER.\nOR IF YOU'RE ALREADY A MEMBER JUST LOGIN\n")
-print("1 -> LOGIN\n2 -> SIGNUP\n3 -> EXIT")
+def login(data):
+    if name in data:
+        if password == data[name]:
+            print("Sign In Complete...\nRedirecting to HomePage")
+            homepage(name)
+        else:
+            print("password not match")
+    else:
+        print("User not found")
 
+
+def signup(data):
+    if name not in data:
+        data[name] = password
+        with open('data.json', 'w') as json_file_write:
+            json.dump(data, json_file_write)
+        print("USER REGISTER... \nNOW LOGIN AGAIN")
+    else:
+        print("user already registered")
+
+
+def homepage(name):
+    seats = {
+        "1A": False,
+        "2A": False,
+        "3A": False,
+        "4A": False,
+        "5A": False,
+        "6A": False,
+        "7A": False,
+        "8A": False,
+        "9A": False,
+        "10A": False
+
+    }
+    print("\n\nWELCOME {} TO JET BOOKING SYSTEM\n".format(name))
+    print("ENTER YOUR DETAILS FOR BOOKING... \n WE ONLY HAVE 10 SEATS ")
+    print("CHOOSE YOUR SEATS FROM BELOW LIST")
+    for s in seats:
+        print(s, end=" ")
+    seat_choice = input("\nENTER YOUR SEAT NUMBER : -")
+    if seat_choice:
+        print("Seat reserved.\nYour seat number is {}".format(seat_choice))
+        seats[seat_choice] = True
+        print(seats)
+        if input("Do you wanna book another seat...( yes / no )").lower() == "yes":
+            do_you_want_to_contimue()
+        else:
+            exit()
+    elif not seat_choice:
+        print("Seat Already Reserved...")
+        if input("Do you wanna book another seat...( yes / no )").lower() == "yes":
+            do_you_want_to_contimue()
+        else:
+            exit()
+    else:
+        print("Wrong Input")
+
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+# MAIN CODE
+print("Welcome to jet booking.\nBefore further proceeding choose one of these : -")
+print("""
+1 - SIGN IN (FOR REGISTERED USER)
+2 - SIGN UP (FOR NEW USER)
+3 - EXIT
+""")
 choice = int(input())
 if choice == 1:
-    name = input()
-    password = input()
-    if Authentication.login(name, password, dictionary):
-        print("Welcome {}".format(name))
-        login_successfull.play_area()
-    else:
-        print("This is not a right credential. \nExiting program ")
-        exit(0)
+    print("Welcome to Sign In Process...")
+    # User input() for signing
+    print("_____Fill all credentials_____")
+    name = input("Enter Your name : - ")
+    password = input("Enter Your password : -")
+    login(data)
+
+
 elif choice == 2:
-    name = input()
-    password = input()
-    if Authentication.signup(name, password, dictionary):
-        login_successfull.play_area()
-    else:
-        print("Exiting Program")
-        exit(0)
+    print("Welcome New User to Signup Process...")
+    # User input() for signing
+    print("_____Fill all credentials_____")
+    name = input("Enter Your name : - ")
+    password = input("Enter Your password : -")
+    signup(data)
+
+
 elif choice == 3:
-    exit(0)
-
-
-
-
-
-
+    exit()
